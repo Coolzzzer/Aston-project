73592bd2ad348e906eae1847936ad6e014002ea6
Daria Vdovina <vdovinadn@gmail.com>
2025-03-04 23:23:48 +0300

    fix: wrong text

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import footerStyle from "./footer.module.css";
 export default function Footer() {
     return (
         <footer className={footerStyle.footer}>
-            © 2020 Nando Putra Pratama
+            © Команда 3
         </footer>
     );
 };
