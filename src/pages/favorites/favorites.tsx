df01797ba71ee05d905484b99c863c1bb7681c25
Daria Vdovina <vdovinadn@gmail.com>
2025-03-10 09:56:45 +0300

    feat: favorites page logic

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@store/store";
 import {
   Card,
   CardContent,
@@ -7,14 +9,17 @@ import {
   Container,
   Stack,
 } from "@mui/material";
-import { collection } from "../../mock/collectionMovies";
 import MovieCard from "@components/movieCard/movieCard";
 import favoritesStyle from "@pages/favorites/favorites.module.css";
 
 const MovieList: React.FC = () => {
+  const favorites = useSelector(
+    (state: RootState) => state.favorites.favorites
+  );
+
   return (
     <Stack className={favoritesStyle.favoritesStack}>
-      <Typography variant="h2" component="div">
+      <Typography variant="h2" component="div" color= 'white'>
         Избранное
       </Typography>
       <Container className={favoritesStyle.favoritesContainer} maxWidth="xl">
@@ -22,17 +27,23 @@ const MovieList: React.FC = () => {
           style={{
             maxHeight: "80vh",
             overflow: "auto",
-            borderRadius: "20px",
-            backgroundColor: "var(--header-footer-bg-color)", // здесь не получается пока вынести стиль в favorites.module.css, так как он кроме параметра max-height не отображает остальные, ищу как это исправить
+            borderRadius: "60px",
+            backgroundColor: "var(--header-footer-bg-color)",
           }}
         >
           <CardContent>
             <Grid2 className={favoritesStyle.grid2} container spacing={2}>
-              {collection.map((movie, index) => (
-                <Grid2 key={index}>
-                  <MovieCard movie={movie} />
-                </Grid2>
-              ))}
+              {favorites.length > 0 ? (
+                favorites.map((movie) => (
+                  <Grid2 key={movie.imdbID}>
+                    <MovieCard movie={movie} />
+                  </Grid2>
+                ))
+              ) : (
+                <Typography variant="h6" component="div">
+                  У вас нет избранных фильмов.
+                </Typography>
+              )}
             </Grid2>
           </CardContent>
         </Card>
@@ -41,4 +52,4 @@ const MovieList: React.FC = () => {
   );
 };
 
-export {MovieList};
+export { MovieList };
