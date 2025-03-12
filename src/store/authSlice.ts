eafec2f1d58d9ce780f6ff994aa0835b0709f83b
Daria Vdovina <vdovinadn@gmail.com>
2025-03-12 23:55:12 +0300

    feat: add validation errors

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -22,21 +22,26 @@ const authSlice = createSlice({
     },
     loginUser: (state, action: PayloadAction<User>) => {
       const storedUser = getLocalStorageItem<User>(STORAGE_KEYS.USER);
-
-      if (
-        storedUser &&
-        storedUser.email === action.payload.email &&
-        storedUser.password === action.payload.password
-      ) {
-        state.user = storedUser;
-        state.loggedIn = true;
-        setLocalStorageItem(STORAGE_KEYS.LOGGEDIN, state.loggedIn);
-        setLocalStorageItem(STORAGE_KEYS.USER, state.user);
-      } else {
-        throw new Error("Неверный email или пароль");
+      if (!storedUser) {
+        throw new Error("Неверный Логин и Пароль");
       }
+
+      const emailMatches = storedUser.email === action.payload.email;
+      const passwordMatches = storedUser.password === action.payload.password;
+
+      if (!emailMatches && !passwordMatches) {
+        throw new Error("Неверный Логин и Пароль");
+      } else if (!emailMatches) {
+        throw new Error("Неверный Логин");
+      } else if (!passwordMatches) {
+        throw new Error("Неверный Пароль");
+      }
+      state.user = storedUser;
+      state.loggedIn = true;
+      setLocalStorageItem(STORAGE_KEYS.LOGGEDIN, state.loggedIn);
+      setLocalStorageItem(STORAGE_KEYS.USER, state.user);
     },
-    
+
     logoutUser: (state) => {
       state.loggedIn = false;
       setLocalStorageItem(STORAGE_KEYS.LOGGEDIN, state.loggedIn);
